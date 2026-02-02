6faa094357f5a7eb8c7371048121e733a6543928
fabian-dedalus-2nd-chance <fabian.bruenger@dedalus.eu>
2026-02-02 16:21:35 +0100

    main: Found out that my method is wrong

--- playground/src/leetcode_functions.rs
+++ playground/src/leetcode_functions.rs
@@ -19,6 +19,7 @@ impl ListNode {
 
 
 pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    println!("Add two numbers called");
     // The number of nodes in each linked list is in the range [1, 100].
     // 0 <= Node.val <= 9
     // It is guaranteed that the list represents a number that does not have leading zeros.
@@ -42,7 +43,7 @@ pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> 
     
     // Getting arrays from linked lists
     while let Some(node) = current_node_l1 {
-        println!("{}", node.val);
+        // println!("{}", node.val);
         // Verify if node value is in range
         if node.val < min_node_value || node.val > max_node_value {
             panic!("Node value is out of bounds");
@@ -53,7 +54,7 @@ pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> 
     }
 
     while let Some(node) = current_node_l2 {
-        println!("{}", node.val);
+        // println!("{}", node.val);
         // Verify if node value is in range
         if node.val < min_node_value || node.val > max_node_value {
             panic!("Node value is out of bounds");
@@ -90,11 +91,19 @@ pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> 
         }
     }
 
-    // Processing:
-    let l1_number = l1_array.iter().rev().fold(0, |acc, &digit| acc * 10 + digit);
-    let l2_number = l2_array.iter().rev().fold(0, |acc, &digit| acc * 10 + digit);
+    // Processing: Result number
+    let l1_number = l1_array.iter().rev().fold(0, |acc: usize, &digit| acc * 10 + digit as usize);
+    let l2_number = l2_array.iter().rev().fold(0, |acc: usize, &digit| acc * 10 + digit as usize);
     let res = l1_number + l2_number;
+    println!("l1 number: {}", l1_number);
+    println!("l2 number: {}", l2_number);
     println!("res number: {}", res);
+    
+    // Edge case for 0 result
+    if res == 0 {
+        return Some(Box::new(ListNode { val: 0, next: None }));
+    } 
+    
     let mut arr  = Vec::new();
     let mut n = res;
     
@@ -115,4 +124,71 @@ pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> 
     }
 
     res_final
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_list(digits: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head = None;
+        for &digit in digits.iter().rev() {
+            head = Some(Box::new(ListNode {
+                val: digit,
+                next: head,
+            }));
+        }
+        head
+    }
+
+    fn list_to_vec(mut head: Option<Box<ListNode>>) -> Vec<i32> {
+        let mut result = Vec::new();
+        while let Some(node) = head {
+            result.push(node.val);
+            head = node.next;
+        }
+        result
+    }
+
+    #[test]
+    fn test_add_two_numbers_342_plus_465() {
+        let l1 = create_list(vec![2, 4, 3]);
+        let l2 = create_list(vec![5, 6, 4]);
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(list_to_vec(result), vec![7, 0, 8]);
+    }
+
+    #[test]
+    fn test_add_two_numbers_zero_plus_zero() {
+        let l1 = create_list(vec![0]);
+        let l2 = create_list(vec![0]);
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(list_to_vec(result), vec![0]);
+    }
+
+    #[test]
+    fn test_add_two_numbers_large_numbers() {
+        let l1 = create_list(vec![9, 9, 9, 9, 9, 9, 9]);
+        let l2 = create_list(vec![9, 9, 9, 9]);
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(list_to_vec(result), vec![8, 9, 9, 9, 0, 0, 0, 1]);
+    }
+
+    #[test]
+    fn test_add_two_numbers_different_lengths() {
+        let l1 = create_list(vec![9]);
+        let l2 = create_list(vec![1, 9, 9, 9, 9, 9, 9, 9, 9, 9]);
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(list_to_vec(result), vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+    }
+
+    #[test]
+    fn test_add_two_numbers_very_large() {
+        // 10000000000000000000000000000001 + 465
+        let l1 = create_list(vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+        let l2 = create_list(vec![5, 6, 4]);
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(list_to_vec(result), vec![6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+    }
 }
