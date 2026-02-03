50c76ea6f4eff0f08f11203019a3a51eff1020c2
Fabian Brünger <52741968+FabianBruenger@users.noreply.github.com>
2026-02-03 15:35:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (55.96%)

---
+++ 0002-add-two-numbers/solution.rs
@@ -0,0 +1,65 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    // println!("Add two numbers called");
+    // The number of nodes in each linked list is in the range [1, 100].
+    // 0 <= Node.val <= 9
+    // It is guaranteed that the list represents a number that does not have leading zeros.
+
+    // Iterate through both lists and add both values with transfer
+    let mut l1_current = l1;
+    let mut l2_current = l2;
+    let mut transfer = 0;
+    let mut res_final: Option<Box<ListNode>> = None;
+    let mut res_final_arra: Vec<i32> = Vec::new();
+
+    while l1_current.is_some() || l2_current.is_some() || transfer != 0 {
+      // println!("In while.. l1 = {:?}, l2 = {:?}, transfer = {}\n", l1_current, l2_current, transfer);
+
+      // get values and add them
+      // Note: The map_or function I needed to look up!
+      let val1 = l1_current.as_ref().map_or(0, |node| node.val);
+      let val2 = l2_current.as_ref().map_or(0, |node| node.val);
+
+      let sum = val1 + val2 + transfer;
+      transfer = sum / 10;
+      let digit = sum % 10;
+
+      // println!("Calculated: sum = val1 + val2 + transfer. {} = {} + {} + {} \n", sum, val1, val2, transfer);
+
+      // Push the digits to the result array, later transform it.
+      res_final_arra.push(digit);
+
+      // Note: The end_then function I needed to look up!
+      l1_current = l1_current.and_then(|node| node.next);
+      l2_current = l2_current.and_then(|node| node.next);
+    }
+
+    // Transform the result array into the linked list
+    // Note: This could maybe be smarter. 
+    for x in res_final_arra.iter().rev() {
+        res_final = Some(Box::new(ListNode {
+            val: *x as i32,
+            next: res_final,
+        }));
+    }
+
+    res_final
+        
+    }
+}
